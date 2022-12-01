0b24c55b89ed015c664791cbeaccb1892b875105
LxLeChat <yanaski@msn.com>
2022-12-01 14:09:59 +0100

    Create Enums.ps1

---
+++ Enums.ps1
@@ -0,0 +1,12 @@
+Enum BasicAttributes {
+    AllowNull = 1
+    AllowEmptyString = 2
+    AllowEmptyCollection = 3
+    ValidateNotNull = 4
+    ValidateNotNullOrEmpty = 5
+    ValidateCount = 6
+    ValidateLength = 7
+    ValidateRange = 8
+    ValidateSet = 9
+    ValidatePattern = 10
+}
